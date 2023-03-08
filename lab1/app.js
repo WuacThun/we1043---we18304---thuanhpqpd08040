d0437b8826c1568c801ba4c32e2bfa8bbd4f59ba
BinK <thuanhpqpd08040@fpt.edu.vn>
2023-03-08 18:20:27 +0700

    nap lab1

---
+++ lab1/app.js
@@ -0,0 +1,9 @@
+// nhap a tu ban phim
+let a = prompt("Nhap a tu ban phim");
+// let a1 = parseInt(a);
+//nhap b tu ban phim
+let b = prompt("Nhap b tu ban phim");
+let c = parseInt(a) + parseInt(b);
+document.write(c);
+
+console.log(c);
